650353ac3003ffc71561d48bc2b3e7f85d759a73
lvhaixiang <lvhaixiang@enn.cn>
2020-06-03 18:04:47 +0800

    修改git测试

--- ssmstudy/src/main/java/com/hansem/controller/UserController.java
+++ ssmstudy/src/main/java/com/hansem/controller/UserController.java
@@ -25,7 +25,9 @@ public class UserController {
 	@RequestMapping(value="/{id}",method=RequestMethod.GET) 
 	public @ResponseBody User getUserInJson(@PathVariable String id) {
 		int a = 1;
+		System.out.println(a);
 		int userId = Integer.parseInt(id);  
+        System.out.println("userId:"+userId);
         User user = this.userService.getUserById(userId); 
         int b = 2;
         System.out.println(b);
